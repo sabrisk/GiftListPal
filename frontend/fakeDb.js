59cab3c2ca7237f1d6521ceb690c2fa347162d64
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-10-01 11:21:14 -0700

    Modifies test data for testing.

--- frontend/fakeDb.js
+++ frontend/fakeDb.js
@@ -1,12 +1,11 @@
 const people = [
-	{ event_id: 1, name: "Javi" },
-	{ event_id: 1, name: "Harley" },
-	{ event_id: 1, name: "Brock" },
-	{ event_id: 2, name: "Mom" },
-	{ event_id: 2, name: "Dad" },
-	{ event_id: 2, name: "Pamela" },
-	{ event_id: 2, name: "Michelle" },
+	{ id: 1, event_id: 1, name: "Javi" },
+	{ id: 2, event_id: 1, name: "Harley" },
+	{ id: 3, event_id: 1, name: "Brock" },
+	{ id: 4, event_id: 2, name: "Mom" },
+	{ id: 5, event_id: 2, name: "Dad" },
+	{ id: 6, event_id: 2, name: "Pamela" },
+	{ id: 7, event_id: 2, name: "Michelle" },
 ];
-let inc = 3;
 
 export default people;
