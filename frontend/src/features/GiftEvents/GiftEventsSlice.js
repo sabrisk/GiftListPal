26aad2b52ff5a049ac29acd0da6dc53c68d56384
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-10-20 11:50:57 -0700

    Cleans up comments in slice.

--- frontend/src/features/GiftEvents/GiftEventsSlice.js
+++ frontend/src/features/GiftEvents/GiftEventsSlice.js
@@ -1,20 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-// import SpaceTravelApi from "../../services/SpaceTravelApi";
 
 const initialState = {
 	list: [],
 	getGiftEventsStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-	// getSpacecraftsStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-	// buildSpacecraftStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-	// destroySpacecraftByIdStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-	// sendSpacecraftToPlanetStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-	// getSpacecraftByIdStatus: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
-
-	// getSpacecraftsError: null,
-	// buildSpacecraftError: null,
-	// destroySpacecraftByIdError: null,
-	// sendSpacecraftToPlanetError: null,
-	// getSpacecraftByIdError: null,
 };
 
 export const getGiftEvents = createAsyncThunk(
@@ -55,61 +43,6 @@ export const postGiftEvent = createAsyncThunk(
 	}
 );
 
-// export const getSpacecraftById = createAsyncThunk(
-// 	"spacecrafts/getSpacecraftById",
-// 	async ({ id }) => {
-// 		try {
-// 			const response = await SpaceTravelApi.getSpacecraftById({ id });
-// 			return [...response.data];
-// 		} catch (err) {
-// 			throw new Error(err.message);
-// 		}
-// 	}
-// );
-
-// export const buildSpacecraft = createAsyncThunk(
-// 	"spacecrafts/buildSpacecraft",
-// 	async (newSpacecraft, { dispatch }) => {
-// 		try {
-// 			const response = await SpaceTravelApi.buildSpacecraft(
-// 				newSpacecraft
-// 			);
-// 			await dispatch(getSpacecrafts());
-// 			return response;
-// 		} catch (err) {
-// 			throw new Error(err.message);
-// 		}
-// 	}
-// );
-
-// export const destroySpacecraftById = createAsyncThunk(
-// 	"spacecrafts/destroySpacecraftById",
-// 	async (id, { dispatch }) => {
-// 		try {
-// 			const response = await SpaceTravelApi.destroySpacecraftById(id);
-// 			await dispatch(getSpacecrafts());
-// 			return response;
-// 		} catch (err) {
-// 			throw new Error(err.message);
-// 		}
-// 	}
-// );
-
-// export const sendSpacecraftToPlanet = createAsyncThunk(
-// 	"spacecrafts/sendSpacecraftToPlanet",
-// 	async ({ spacecraftId, originPlanetId, targetPlanetId, capacity }) => {
-// 		try {
-// 			const response = await SpaceTravelApi.sendSpacecraftToPlanet({
-// 				spacecraftId,
-// 				targetPlanetId,
-// 			});
-// 			return { spacecraftId, originPlanetId, targetPlanetId, capacity };
-// 		} catch (err) {
-// 			throw new Error(err.message);
-// 		}
-// 	}
-// );
-
 const giftEventsSlice = createSlice({
 	name: "giftEvents",
 	initialState,
@@ -139,57 +72,6 @@ const giftEventsSlice = createSlice({
 				state.postGiftEventStatus = "failed";
 				state.postGiftEventError = action.error.message;
 			});
-
-		// 		.addCase(getSpacecraftById.pending, (state, action) => {
-		// 			state.getSpacecraftByIdStatus = "loading";
-		// 		})
-		// 		.addCase(getSpacecraftById.fulfilled, (state, action) => {
-		// 			state.getSpacecraftByIdStatus = "succeeded";
-		// 			state.list = action.payload;
-		// 		})
-		// 		.addCase(getSpacecraftById.rejected, (state, action) => {
-		// 			state.getSpacecraftByIdStatus = "failed";
-		// 			state.getSpacecraftByIdError = action.error.message;
-		// 		})
-
-		// 		.addCase(buildSpacecraft.pending, (state, action) => {
-		// 			state.buildSpacecraftStatus = "loading";
-		// 		})
-		// 		.addCase(buildSpacecraft.fulfilled, (state, action) => {
-		// 			state.buildSpacecraftStatus = "succeeded";
-		// 		})
-		// 		.addCase(buildSpacecraft.rejected, (state, action) => {
-		// 			state.buildSpacecraftStatus = "failed";
-		// 			state.buildSpacecraftError = action.error.message;
-		// 		})
-		// 		.addCase(destroySpacecraftById.pending, (state, action) => {
-		// 			state.destroySpacecraftByIdStatus = "loading";
-		// 		})
-		// 		.addCase(destroySpacecraftById.fulfilled, (state, action) => {
-		// 			state.destroySpacecraftByIdStatus = "succeeded";
-		// 		})
-		// 		.addCase(destroySpacecraftById.rejected, (state, action) => {
-		// 			state.destroySpacecraftByIdStatus = "failed";
-		// 			state.destroySpacecraftByIdError = action.error.message;
-		// 		})
-		// 		.addCase(sendSpacecraftToPlanet.pending, (state, action) => {
-		// 			state.sendSpacecraftToPlanetStatus = "loading";
-		// 		})
-		// 		.addCase(sendSpacecraftToPlanet.fulfilled, (state, action) => {
-		// 			state.sendSpacecraftToPlanetStatus = "succeeded";
-		// 			const spacecraftToUpdate = state.list.find(
-		// 				(craft) => craft.id === action.payload.spacecraftId
-		// 			);
-
-		// 			if (spacecraftToUpdate) {
-		// 				spacecraftToUpdate.currentLocation =
-		// 					action.payload.targetPlanetId;
-		// 			}
-		// 		})
-		// 		.addCase(sendSpacecraftToPlanet.rejected, (state, action) => {
-		// 			state.sendSpacecraftToPlanetStatus = "failed";
-		// 			state.sendSpacecraftToPlanetError = action.error.message;
-		// 		});
 	},
 });
 
