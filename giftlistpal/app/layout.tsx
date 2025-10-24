e9d9140dec5ab8ef6f4ed55f545a99f5640e4c21
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-10-24 15:30:03 -0700

    Adds sign out functionality.

--- giftlistpal/app/layout.tsx
+++ giftlistpal/app/layout.tsx
@@ -4,6 +4,12 @@ import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "@/store/store"; // adjust path if needed
 import { SessionProvider } from "next-auth/react";
+import { signOut } from "next-auth/react";
+
+const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+	e.preventDefault();
+	await signOut({ callbackUrl: "/signup" });
+};
 
 export default function RootLayout({
 	children,
@@ -12,11 +18,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>
-				<SessionProvider>
+			<SessionProvider>
+				<body>
+					<button
+						onClick={handleClick}
+						className="hidden md:block bg-[#F5EFE7] text-[#313131] font-bold border-1 rounded border-[#F5EFE7] px-2 "
+					>
+						Sign Out
+					</button>
 					<Provider store={store}>{children}</Provider>
-				</SessionProvider>
-			</body>
+				</body>
+			</SessionProvider>
 		</html>
 	);
 }
--- giftlistpal/app/signup/page.tsx
+++ giftlistpal/app/signup/page.tsx
@@ -1,77 +1,41 @@
 "use client";
 import { signIn } from "next-auth/react";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function SignUp() {
+	const router = useRouter();
+	const { data: session } = useSession();
 	const [user, setUser] = useState({
-		// name: "",
 		email: "",
-		// password: "",
-		// confirmPassword: "",
 	});
 
-	const resendAction = (formData: FormData) => {
-		signIn("resend", user);
+	const resendAction = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const res = await signIn("resend", { ...user, redirect: false });
+		console.log(res);
 	};
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
-		console.log(e.target);
+		console.log(name, value);
 		setUser((prev) => ({ ...prev, [name]: value }));
-		console.log(user);
 	};
 
-	// const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-	// 	e.preventDefault();
-
-	// 	if (user.password !== user.confirmPassword) {
-	// 		alert("Passwords do no match!");
-	// 		return;
-	// 	}
-
-	// 	localStorage.setItem("user", JSON.stringify(user));
-
-	// 	try {
-	// 		const response = await fetch("http://localhost:3001/signup", {
-	// 			method: "POST",
-	// 			headers: {
-	// 				Accept: "application/json",
-	// 				"Content-Type": "application/json",
-	// 			},
-	// 			body: JSON.stringify(user),
-	// 		});
-
-	// 		if (!response.ok) {
-	// 			const error = await response.text();
-	// 			alert(`Signup failed: ${error}`);
-	// 			return;
-	// 		}
-
-	// 		const jsonResponse = await response.json();
-	// 		// do other things like redirect users to another page
-	// 	} catch (error) {
-	// 		alert("An error occurred");
-	// 	}
-	// };
+	useEffect(() => {
+		if (session) {
+			router.push("/events");
+		}
+	}, [session, router]);
 
 	return (
 		<div>
 			<h1 className="text-3xl p-3 bg-gray-800">Sign up</h1>
 			<form
-				action={resendAction}
+				onSubmit={resendAction}
 				className="flex flex-col p-3 gap-4 mt-6 max-w-sm mx-auto"
 			>
-				{/* <label className="flex flex-col">
-					<span className="mb-1 font-medium">Name</span>
-					<input
-						onChange={handleChange}
-						type="text"
-						name="name"
-						className="border rounded px-3 py-2 bg-gray-900 text-white"
-						required
-						value={user.name}
-					/>
-				</label> */}
 				<label className="flex flex-col">
 					<span className="mb-1 font-medium">Email</span>
 					<input
@@ -84,32 +48,7 @@ export default function SignUp() {
 						value={user.email}
 					/>
 				</label>
-				{/* <label className="flex flex-col">
-					<span className="mb-1 font-medium">Password</span>
-					<input
-						onChange={handleChange}
-						type="password"
-						name="password"
-						className="border rounded px-3 py-2 bg-gray-900 text-white"
-						required
-						autoComplete="new-password"
-						value={user.password}
-					/>
-				</label>
-				<label className="flex flex-col">
-					<span className="mb-1 font-medium">Confirm Password</span>
-					<input
-						onChange={handleChange}
-						type="password"
-						name="confirmPassword"
-						className="border rounded px-3 py-2 bg-gray-900 text-white"
-						required
-						autoComplete="new-password"
-						value={user.confirmPassword}
-					/>
-				</label> */}
 				<button
-					// onClick={handleClick}
 					type="submit"
 					className="mt-4 bg-blue-700 hover:bg-blue-800 text-white font-semibold py-2 rounded"
 				>
