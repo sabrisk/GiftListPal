178c2e39c9b17e911ffa2ba21e9d91666cf41629
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-11-04 22:35:29 -0800

    Removes unneeded types and imports.

--- giftlistpal/src/types/event.ts
+++ giftlistpal/src/types/event.ts
@@ -1,5 +1,5 @@
 import { Prisma } from "@prisma/client";
-import { Event as PrismaEvent } from "@prisma/client";
+import { eventSelect } from "@lib/prismaSelects";
 
 export interface CreateEventRequest {
 	name: string;
@@ -18,11 +18,6 @@ export interface ErrorResponse {
 	code: string;
 	message: string;
 }
-
-export type EventsResponse = SuccessResponse<PrismaEvent[]>;
-export type EventResponse = SuccessResponse<PrismaEvent>;
-
-import { eventSelect } from "@lib/prismaSelects";
 
 export type Event = Prisma.EventGetPayload<{
 	select: typeof eventSelect;
