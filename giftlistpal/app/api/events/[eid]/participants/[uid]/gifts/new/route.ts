53ae8d3884522c2b87bce51719eed83c7cf44cad
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-12-03 08:58:12 -0800

    Removes unneeded code.

--- giftlistpal/app/api/events/[eid]/participants/[uid]/gifts/new/route.ts
+++ giftlistpal/app/api/events/[eid]/participants/[uid]/gifts/new/route.ts
@@ -20,48 +20,8 @@ const errorResponse = (code: string, message: string): ErrorResponse => ({
 	message,
 });
 
-// export async function GET() {
-// 	try {
-// 		console.log("GET /api/events called");
-// 		const session = await auth();
-
-// 		if (!session?.user?.id) {
-// 			return NextResponse.json(
-// 				errorResponse("UNAUTHORIZED", "Unauthorized"),
-// 				{ status: 401 }
-// 			);
-// 		}
-
-// 		console.log("Fetching events for user:", session.user.id);
-// 		const events = await prisma.event.findMany({
-// 			where: { participants: { some: { userId: session.user.id } } },
-// 			select: {
-// 				id: true,
-// 				name: true,
-// 				date: true,
-// 				description: true,
-// 				ownerId: true,
-// 				createdAt: true,
-// 			},
-// 		});
-// 		return NextResponse.json(
-// 			successResponse(events, "Events retreived successfully"),
-// 			{
-// 				status: 200,
-// 			}
-// 		);
-// 	} catch (err) {
-// 		console.error("Unhandled error in event route:", err);
-// 		return NextResponse.json(
-// 			errorResponse("INTERNAL_ERROR", "Internal server error"),
-// 			{ status: 500 }
-// 		);
-// 	}
-// }
-
 export async function POST(req: Request) {
 	try {
-		console.log("made it to gift post");
 		const session = await auth();
 		if (!session?.user?.id) {
 			return NextResponse.json(
@@ -69,15 +29,6 @@ export async function POST(req: Request) {
 				{ status: 401 }
 			);
 		}
-		const userId = session.user.id;
-
-		// (alias) interface CreateGiftRequestPost {
-		//     name: string;
-		//     link?: string | undefined;
-		//     eventId: number;
-		//     participantId: string;
-		//     addedByUserId: string;
-		// }
 
 		const {
 			name,
@@ -97,7 +48,6 @@ export async function POST(req: Request) {
 			);
 		}
 
-		// const event = await prisma.$transaction(async (tx) => {
 		const newGift = await prisma.gift.create({
 			data: {
 				name,
@@ -109,43 +59,17 @@ export async function POST(req: Request) {
 			},
 		});
 
-		// await tx.eventParticipant.create({
-		// 	data: {
-		// 		userId,
-		// 		eventId: event.id,
-		// 		isShopper: false,
-		// 		isRecipient: false,
-		// 	},
-		// });
-
-		// const fullEvent = await tx.event.findUnique({
-		// 	where: { id: event.id },
-		// 	select: {
-		// 		id: true,
-		// 		name: true,
-		// 		date: true,
-		// 		description: true,
-		// 		ownerId: true,
-		// 		createdAt: true,
-		// 	},
-		// });
-
-		// return fullEvent;
-		// });
-
 		if (!newGift) {
 			return NextResponse.json(
 				errorResponse("NOT_FOUND", "Gift could not be created"),
 				{ status: 500 }
 			);
 		}
-		console.log("gift added successfully", newGift);
 		return NextResponse.json(
 			successResponse(newGift, "Gift added successfully"),
 			{ status: 201 }
 		);
 	} catch (err) {
-		console.error("Error inserting gift:", err);
 		return NextResponse.json(
 			errorResponse("INTERNAL_ERROR", "Internal server error"),
 			{ status: 500 }
