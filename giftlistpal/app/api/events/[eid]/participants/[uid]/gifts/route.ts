3961f3d14defe0e6eeedb571864fa752b84b6549
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-12-03 08:49:28 -0800

    Removes console logs and comments.

--- giftlistpal/app/api/events/[eid]/participants/[uid]/gifts/route.ts
+++ giftlistpal/app/api/events/[eid]/participants/[uid]/gifts/route.ts
@@ -28,7 +28,6 @@ export async function GET(
 ) {
 	try {
 		const { eid, uid } = await params;
-		console.log("GET /api/events/${eventId}/participants/${userId} called");
 		const session = await auth();
 
 		if (!session?.user?.id) {
@@ -38,7 +37,6 @@ export async function GET(
 			);
 		}
 
-		console.log("Fetching gifts for participant:", eid, uid);
 		const gifts = await prisma.gift.findMany({
 			where: {
 				eventId: parseInt(eid),
@@ -96,7 +94,6 @@ export async function PATCH(request: Request) {
 		if (reservedByUserId !== undefined)
 			data.reservedByUserId = reservedByUserId;
 
-		console.log("just prior to update the gift", data);
 		// Update the gift
 		const updatedGift = await prisma.gift.update({
 			///this needs to be a transaction where you get the gift to see if it already has the reservedByUserId set by a different user first
@@ -118,83 +115,6 @@ export async function PATCH(request: Request) {
 	}
 }
 
-// export async function POST(req: Request) {
-// 	try {
-// 		const session = await auth();
-// 		if (!session?.user?.id) {
-// 			return NextResponse.json(
-// 				errorResponse("UNAUTHORIZED", "Unauthorized"),
-// 				{ status: 401 }
-// 			);
-// 		}
-// 		const userId = session.user.id;
-
-// 		const { name, date, description }: CreateEventRequest =
-// 			await req.json();
-
-// 		if (!name || !date || !description) {
-// 			return NextResponse.json(
-// 				errorResponse(
-// 					"BAD_REQUEST",
-// 					"Missing name, date, or description"
-// 				),
-// 				{ status: 400 }
-// 			);
-// 		}
-
-// 		const event = await prisma.$transaction(async (tx) => {
-// 			const event = await tx.event.create({
-// 				data: {
-// 					name,
-// 					date: new Date(date),
-// 					description,
-// 					ownerId: userId,
-// 				},
-// 			});
-
-// 			await tx.eventParticipant.create({
-// 				data: {
-// 					userId,
-// 					eventId: event.id,
-// 					isShopper: false,
-// 					isRecipient: false,
-// 				},
-// 			});
-
-// 			const fullEvent = await tx.event.findUnique({
-// 				where: { id: event.id },
-// 				select: {
-// 					id: true,
-// 					name: true,
-// 					date: true,
-// 					description: true,
-// 					ownerId: true,
-// 					createdAt: true,
-// 				},
-// 			});
-
-// 			return fullEvent;
-// 		});
-
-// 		if (!event) {
-// 			return NextResponse.json(
-// 				errorResponse("NOT_FOUND", "Event could not be created"),
-// 				{ status: 500 }
-// 			);
-// 		}
-
-// 		return NextResponse.json(
-// 			successResponse(event, "Event added successfully"),
-// 			{ status: 201 }
-// 		);
-// 	} catch (err) {
-// 		console.error("Error inserting event:", err);
-// 		return NextResponse.json(
-// 			errorResponse("INTERNAL_ERROR", "Internal server error"),
-// 			{ status: 500 }
-// 		);
-// 	}
-// }
 export async function OPTIONS() {
 	return NextResponse.json(
 		{ error: "Method Not Allowed" },
